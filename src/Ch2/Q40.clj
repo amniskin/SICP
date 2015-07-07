94985973b48f9871064b631372f45b6614825cec
amniskin <amniskin@MicroFsh.PK5001Z>
2015-07-06 21:07:50 -0400

    auto commit

--- src/Ch2/Q40.clj
+++ src/Ch2/Q40.clj
@@ -1,1 +1,8 @@
-;test
+(defn null? [x] (= () x))
+(defn accumulate [op initial sequen]
+  (if (null? sequen) initial
+    (op (first sequen)
+        (accumulate op initial (rest sequen)))))
+(defn flatmap [proc items]
+  (accumulate append nil (map proc items)))
+
