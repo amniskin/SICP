582ed44cc9561fea7ba35567f74b7f645f2fe0c0
amniskin <amniskin@MicroFsh.PK5001Z>
2015-07-12 08:55:46 -0400

    auto commit

--- src/Ch2/Q41.clj
+++ src/Ch2/Q41.clj
@@ -7,11 +7,11 @@
   (accumulate concat nil (map proc items)))
 (defn unique-pairs [n]
   (defn make-pairs [i]
-    (map #(list i (inc %)) (range i)))
+    (map #(list i %) (range 1 i)))
   (flatmap #(make-pairs (inc %)) (range n)))
 (defn unique-triples [n]
   (defn make-triples [pairs]
-    (map #(list (first pairs) (nth pairs 1) (inc %)) (range (nth pairs 1))))
+    (map #(list (first pairs) (nth pairs 1) %) (range 1 (nth pairs 1))))
   (flatmap #(make-triples %) (unique-pairs n)))
 (defn triples-less-than [n s]
   (filter #(= (reduce + %) s) 
--- src/Ch2/Q42.clj
+++ src/Ch2/Q42.clj
@@ -1,1 +1,34 @@
+(defn null? [x] (= () x))
+(defn accumulate [op initial sequen]
+  (if (null? sequen) initial
+    (op (first sequen)
+        (accumulate op initial (rest sequen)))))
+(defn flatmap [proc items]
+  (accumulate concat nil (map proc items)))
+(defn unique-pairs [n]
+  (defn make-pairs [i]
+    (map #(list i %) (range 1 i)))
+  (flatmap #(make-pairs (inc %)) (range n)))
+(defn make-pairs-1-n [i n]
+  (map #(list i %) (range 1 (inc n))))
+(defn add-new-pairs [items i n]
+  (flatmap #(concat items %) (make-pairs-1-n i n)))
+(defn in-check? [one two]
+  (or (= (first one) (first two)) ;same column
+      (= (second one) (second two)) ;same row
+      (= (+ (first one) (second one))
+         (+ (first two) (second two))) ;downward diagonal
+      (= (- (first one) (second one))
+         (- (first two) (second two))))) ;upward diagonal
+(defn good-to-go? [items]
+  (accumulate #(and %1 %2) true (map #(not (in-check? (first items) %)) (rest items))))
+(defn make-em [items i n]
+  (map #(concat (list %) items) (make-pairs-1-n i n)))
+(defn n-queens-solutions [n]
+  (defn make-em [items i]
+    (map #(concat (list %) items) (make-pairs-1-n i n)))
+  (defn doit [items i]
+    (if (> i n) items
+        (doit (filter #(good-to-go? %) (flatmap #(make-em % i) items)) (inc i))))
+  (doit (make-em () 1) 2))
 
